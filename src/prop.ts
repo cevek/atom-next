2c2a3b731e90f8d5d6f07b028bcbf8c46e1418db
cevek <cevek@ya.ru>
2016-11-17 19:55:23 +0300

    Patch with localstorage flag atom_pretty Object.getOwnPropertyNames and Object.keys to show more pretty objects with atoms

--- src/prop.ts
+++ src/prop.ts
@@ -1,10 +1,11 @@
 import {Atom} from "./index";
 (window as any).AtomGlob = Atom;
 
-export function getAtomFieldName(prop:string) {
-    return '_' + prop;
+const atomPrefix = '_';
+export function getAtomFieldName(prop: string) {
+    return atomPrefix + prop;
 }
-export var prop:any = function (proto:any, prop:string, descriptor?:PropertyDescriptor) {
+export var prop: any = function (proto: any, prop: string, descriptor?: PropertyDescriptor) {
     var _prop = getAtomFieldName(prop);
     const fieldName = proto.constructor.name + '.' + prop;
 
@@ -52,45 +53,68 @@ export var prop:any = function (proto:any, prop:string, descriptor?:PropertyDesc
     }
 };
 /*
-const orig = Object.getOwnPropertyDescriptor;
-Object.getOwnPropertyDescriptor = function (obj:any, prop:string) {
-    var result = orig.apply(this, arguments);
-    if (obj && obj['_' + prop] instanceof Atom) {
-        return {
-            configurable: true,
-            enumerable: true,
-            value: obj['_' + prop].value
+ const orig = Object.getOwnPropertyDescriptor;
+ Object.getOwnPropertyDescriptor = function (obj:any, prop:string) {
+ var result = orig.apply(this, arguments);
+ if (obj && obj['_' + prop] instanceof Atom) {
+ return {
+ configurable: true,
+ enumerable: true,
+ value: obj['_' + prop].value
+ }
+ }
+ if (result.get && result.get.AtomGetter) {
+ delete result.get;
+ delete result.set;
+ result.value = null;
+ }
+ return result;
+ }
+
+ const ObjectKeys = Object.keys;
+ Object.keys = function (obj:any) {
+ var result = ObjectKeys.apply(this, arguments);
+ for (var i = 0; i < result.length; i++) {
+ var prop = result[i];
+ if (prop[0] == '_' && prop instanceof Atom) {
+ result.splice(i, 1);
+ // result.push(prop.substr(1));
+ }
+ }
+ return [];
+ }*/
+
+const HostObjectKeys = Object.keys;
+const ObjectKeys = function (obj: any) {
+    const result = HostObjectKeys.apply(this, arguments);
+    const newResult: string[] = [];
+    for (let i = 0; i < result.length; i++) {
+        const prop = result[i];
+        if (prop.substr(0, atomPrefix.length) == atomPrefix) {
+            newResult.push(prop.substr(atomPrefix.length));
+        } else {
+            newResult.push(prop);
         }
     }
-    if (result.get && result.get.AtomGetter) {
-        delete result.get;
-        delete result.set;
-        result.value = null;
-    }
-    return result;
+    return newResult;
 }
 
-const ObjectKeys = Object.keys;
-Object.keys = function (obj:any) {
-    var result = ObjectKeys.apply(this, arguments);
-    for (var i = 0; i < result.length; i++) {
-        var prop = result[i];
-        if (prop[0] == '_' && prop instanceof Atom) {
-            result.splice(i, 1);
-            // result.push(prop.substr(1));
+const HostObjectGetOwnPropertyNames = Object.getOwnPropertyNames;
+const ObjectGetOwnPropertyNames = function (obj: any) {
+    const result = HostObjectGetOwnPropertyNames.apply(this, arguments);
+    const newResult: string[] = [];
+    for (let i = 0; i < result.length; i++) {
+        const prop = result[i];
+        if (prop.substr(0, atomPrefix.length) == atomPrefix) {
+            newResult.push(prop.substr(atomPrefix.length));
+        } else {
+            newResult.push(prop);
         }
     }
-    return [];
-}*/
+    return newResult;
+}
 
-const ObjectGetOwnPropertyNames = Object.getOwnPropertyNames;
-Object.getOwnPropertyNames = function(obj){
-    var result = ObjectGetOwnPropertyNames.apply(this, arguments);
-    for (var i = 0; i < result.length; i++) {
-        var prop = result[i];
-        if (prop[0] == '_' && obj[prop] instanceof Atom) {
-            result.push(prop.substr(1));
-        }
-    }
-    return result;
+if (typeof localStorage == 'object' && localStorage.getItem('atom_pretty')) {
+    Object.getOwnPropertyNames = ObjectGetOwnPropertyNames;
+    Object.keys = ObjectKeys;
 }
